f965bc9355e418b8d1f5e77f744fe41dbf1b6449
UkiOnna <sikirovtemirlan@mail.ru>
2018-05-21 18:22:29 +0600

    aha

---
+++ ParallelLink/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(i);
+
+            }
+            numbers.AsParallel().Where(number => number % 2 == 0).ForAll(number => Console.WriteLine("Поток: "+Thread.CurrentThread.ManagedThreadId + " " + number));
+
+
+
+
+        }
+
+    }
+}
